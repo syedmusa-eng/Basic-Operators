8ad787ebf20259b852c408ef3d91cd6c644147dd
SYED MUSA RAZA <79834398+syedmusa-eng@users.noreply.github.com>
2021-04-07 00:06:04 +0200

    Source code for operators and abbreviations

---
+++ Main.java
@@ -0,0 +1,84 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2; // 1+2 = 3
+        System.out.println("1+2 = " + result);
+        int previousResult = result;
+        System.out.println("previousResult =  " + previousResult);
+        result = result - 1;
+        System.out.println("3 - 1 = " + result);
+        System.out.println("previousResult =  " + previousResult);
+
+        result = result * 10;
+        System.out.println("2 * 10 = " + result);
+        result = result / 5 ;
+        System.out.println("20 / 5 = " + result);
+
+        result = result % 3;  //give me the remainder
+        System.out.println("4 % 3 = " + result);
+
+        //result = result + 1;
+        result ++;
+        System.out.println("result + 1 =" + result);
+
+        result -- ;
+        System.out.println("2 - 1 = " + result);
+
+        //result = result + 2;
+        result +=2;
+        System.out.println("1 + 2 = " + result);
+
+        //result = result * 10;
+        result *= 10;
+        System.out.println("3 * 10 =" + result);
+
+        //result = result / 3;
+        result /= 3;
+        System.out.println(("30 / 3 = " + result));
+
+        //result = result - 2;
+        result -=2;
+        System.out.println("10 - 2 = " + result);
+
+        //result = result + 10;
+        result +=10;
+        System.out.println("8 + 10 = " + result);
+
+        boolean isAlien = false;
+        if(isAlien == false) {
+            System.out.println("It is not an alien!");
+            System.out.println("And I am scared of aliens");
+        }
+
+        int topScore = 80;
+        if(topScore < 100){
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore = 95;
+        if((topScore > secondTopScore) && (topScore < 100)){
+            System.out.println("Greater than second top score and less than 100.");
+        }
+
+        if((topScore > 90) || (secondTopScore <= 90)){
+            System.out.println("Either or both of the conditions are true.");
+        }
+
+        int newValue = 50;
+        if(newValue == 50){
+            System.out.println("This is not an error.");
+        }
+
+        boolean isCar = false;
+        if(!isCar){         //if(isCar != true)
+            System.out.println("This is not suppose to happen.");
+        }
+
+        if(isCar){          //if(isCar == true)
+            System.out.println("Flag is true.");
+        }
+    }
+}
